46056afe001f6643e2547afe25e9850c5b574d3e
nvhiii <nahikhan16@gmail.com>
2025-05-11 22:42:24 -0400

    starting prereq

---
+++ project-1-crud-app/my-journal-dapp/anchor/programs/crud-app/src/lib.rs
@@ -0,0 +1,11 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("HsBtyhQe2fdyWL3ebYWznRtjKe31ueNt3GMRzCRmTenU");
+
+#[program]
+pub mod counter {
+    use super::*;
+
+}
