d5e07b70562e63136fa0c3229e2a533ba1014ca2
nvhiii <nahikhan16@gmail.com>
2025-05-11 23:46:45 -0400

    accounts model

--- project-1-crud-app/my-journal-dapp/anchor/programs/crud-app/src/lib.rs
+++ project-1-crud-app/my-journal-dapp/anchor/programs/crud-app/src/lib.rs
@@ -8,4 +8,47 @@ declare_id!("HsBtyhQe2fdyWL3ebYWznRtjKe31ueNt3GMRzCRmTenU");
 pub mod counter {
     use super::*;
 
+    // instruction handlers
+    pub fn creaste_journal_entry(ctx: Context<CreateEntry>, ) -> Result<()> {
+
+    }
+
+}
+
+#[derive(Accounts)]
+pub struct CreateEntry<'info> {
+
+  #[account(
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    // always include the bump when defining the seed
+    bump,
+    space = 8 + JournalEntryState::INIT_SPACE, // 8 bytes is alloc for the anchor discrim size always
+    payer = owner,
+  )]
+
+  pub journal_entry: Account<'info, JournalEntryState>,
+
+  // since the owner is the one paying, the state of acc changes, hence this is mutable
+  #[account(mut)]
+  pub owner: Signer<'info>,
+
+  pub system_program: Program<'info, System>,
+
+
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState {
+
+  pub owner: Pubkey,
+
+  #[max_len(50)]
+  pub title: String,
+
+  #[max_len(1000)]
+  pub message: String,
+
+
 }
